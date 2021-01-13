8118b99a6500214261da72cd610ee8cdca84129c
ian <ianruizlopez1@gmail.com>
2021-01-13 09:18:34 -0500

    version 1

--- vista/paciente/vista_paciente_listar.php
+++ vista/paciente/vista_paciente_listar.php
@@ -1,7 +1,7 @@
 <div class="col-md-12">
     <div class="box box-warning box-solid">
         <div class="box-header with-border">
-              <h3 class="box-title">BIENVENIDO AL CONTENIDO DEL USUARIO</h3>
+              <h3 class="box-title">BIENVENIDO AL CONTENIDO DEL PACIENTE</h3>
 
             <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
@@ -11,7 +11,7 @@
         </div>
             <!-- /.box-header -->
             <div class="box-body">
-              CONTENIDO USUARIO
+              CONTENIDO PACIENTE
             </div>
             <!-- /.box-body -->
     </div>
