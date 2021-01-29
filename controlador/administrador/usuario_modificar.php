003cc69167d8b573636b87918989d8b83471feaf
ianrl10 <77280931+ianrl10@users.noreply.github.com>
2021-01-28 20:46:16 -0500

    version 6

--- controlador/administrador/usuario_modificar.php
+++ controlador/administrador/usuario_modificar.php
@@ -13,7 +13,8 @@
     $ciudad = htmlspecialchars($_POST['ciudad'], ENT_QUOTES, 'UTF-8');
     $direccion = htmlspecialchars($_POST['direccion'], ENT_QUOTES, 'UTF-8');
     $telefono = htmlspecialchars($_POST['telefono'], ENT_QUOTES, 'UTF-8');
-    $receta = htmlspecialchars($_POST['receta'], ENT_QUOTES, 'UTF-8');
-    $consulta = $MA-> Modificar_Datos_Usuario($idusuario,$rol,$apellido,$sexo,$cedula,$email,$estado,$ciudad,$direccion,$telefono,$receta);
+    $medicamento = htmlspecialchars($_POST['medicamento'], ENT_QUOTES, 'UTF-8');
+    $indicaciones = htmlspecialchars($_POST['indicaciones'], ENT_QUOTES, 'UTF-8');
+    $consulta = $MA-> Modificar_Datos_Usuario($idusuario,$rol,$apellido,$sexo,$cedula,$email,$estado,$ciudad,$direccion,$telefono,$medicamento,$indicaciones);
     echo $consulta;
 ?>
--- controlador/administrador/usuario_registro.php
+++ controlador/administrador/usuario_registro.php
@@ -14,7 +14,8 @@
     $ciudad = htmlspecialchars($_POST['ciudad'], ENT_QUOTES, 'UTF-8');
     $direccion = htmlspecialchars($_POST['direccion'], ENT_QUOTES, 'UTF-8');
     $telefono = htmlspecialchars($_POST['telefono'], ENT_QUOTES, 'UTF-8');
-    $receta = htmlspecialchars($_POST['receta'], ENT_QUOTES, 'UTF-8');
-    $consulta = $MA-> Registrar_Usuario($usuario,$rol,$apellido,$contra,$sexo,$cedula,$email,$estado,$ciudad,$direccion,$telefono,$receta);
+    $medicamento = htmlspecialchars($_POST['medicamento'], ENT_QUOTES, 'UTF-8');
+    $indicaciones = htmlspecialchars($_POST['indicaciones'], ENT_QUOTES, 'UTF-8');
+    $consulta = $MA-> Registrar_Usuario($usuario,$rol,$apellido,$contra,$sexo,$cedula,$email,$estado,$ciudad,$direccion,$telefono,$medicamento,$indicaciones);
     echo $consulta;
 ?>
